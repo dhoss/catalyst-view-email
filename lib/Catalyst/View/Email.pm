6e47d959e4ee040309e5cb78f2aaa7c4df7ea889
Devin Austin <devin.austin@gmail.com>
2013-02-13 13:13:08 -0700

    bumped version

--- lib/Catalyst/View/Email.pm
+++ lib/Catalyst/View/Email.pm
@@ -8,7 +8,7 @@ use Email::Sender::Simple qw/ sendmail /;
 use Email::MIME::Creator;
 extends 'Catalyst::View';
 
-our $VERSION = '0.32';
+our $VERSION = '0.33';
 $VERSION = eval $VERSION;
 
 has 'mailer' => (
